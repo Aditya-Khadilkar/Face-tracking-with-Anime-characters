1048439345c6a77704f8211f0b8dad6b5c41de71
Aditya Khadilakr <ka.aditya1@gmail.com>
2019-06-27 17:07:21 +0530

    Update YURI FULLSCREEN.py

--- YURI FULLSCREEN.py
+++ YURI FULLSCREEN.py
@@ -88,10 +88,10 @@ def rotate_point(pos, img, angle):
 gameDisplay = show_yuri_image(scl, scb)
 running = True
 while running:
-##    event = pygame.event.wait ()
-##    pressed = pygame.key.get_pressed()
-##    if(pressed[pygame.K_q]):
-##        running = False
+    event = pygame.event.wait ()
+    pressed = pygame.key.get_pressed()
+    if(pressed[pygame.K_q]):
+        running = False
 
     ret, imgn = camera.read()
     img = cv2.flip(imgn, +1)
